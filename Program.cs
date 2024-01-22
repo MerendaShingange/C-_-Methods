a0c97554d1f561ad0a544d8f77d11dd761147ee3
Lenovo-User <Lenovo-User@DESKTOP-TIU29D7>
2024-01-22 19:55:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C___Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHi();
+            // methods with  parameters
+            sayHiUser("Merenda");
+            sayHiAge("John", 55);
+            Console.ReadLine();
+
+
+
+        }
+        //creating a method .ignoring static for now, use capital as per conventions.
+        static void SayHi()
+        {
+            Console.WriteLine("HI");
+            //METHOD WITH MORE THAN ONE PARAMETER
+
+            static void SayHiUser(string name)
+            {
+                Console.WriteLine("Hello" + name);
+
+            }
+        }
+        //method with many parameters
+        static void SatHiAge(string name, int age)
+        {
+            Console.WriteLine("hello" + name + "you are " + age);
+        }
+    }
+}
